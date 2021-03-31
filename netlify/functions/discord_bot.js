b9bf988b294039cecf49f560e70e0adf4916237c
Kyle Schiller <kgschiller@gmail.com>
2021-03-31 01:11:47 -0400

    try discord bot

---
+++ netlify/functions/discord_bot.js
@@ -0,0 +1,20 @@
+const { verifyKey } = require('discord-interactions');
+
+exports.handler = async function(event, context) {
+    console.log(event);
+    const signature = event.headers['x-signature-ed25519'];
+    const timestamp = event.headers['x-signature-timestamp'];
+    console.log(signature, timestamp);
+    console.log(process.env['DISCORD_PUBLIC_KEY']);
+    const isValidRequest = verifyKey(event.body, signature, timestamp, process.env['DISCORD_PUBLIC_KEY'])
+    if (!isValidRequest) {
+        return {
+            statusCode: 401,
+            body: 'Bad request signature'
+        };
+    }
+    return {
+        statusCode: 200,
+        body: JSON.stringify({ message: "Brand" })
+    };
+}
