3d406721289ead97914c2de53b41b290bd393525
Kyle Schiller <kgschiller@gmail.com>
2021-03-31 02:06:15 -0400

    verify signature

--- api/discord_bot.js
+++ api/discord_bot.js
@@ -4,7 +4,14 @@ module.exports = (req, res) => {
     console.log(req.headers);
     console.log(req.query);
     console.log(req.body);
-    console.log(process.env['DISCORD_PUBLIC_KEY'])
+    const signature = req.headers['x-signature-ed25519'];
+    const timestamp = req.headers['x-signature-timestamp'];
+    console.log(signature, timestamp);
+    console.log(process.env['DISCORD_PUBLIC_KEY']);
+    const isValidRequest = verifyKey(req.body, signature, timestamp, process.env['DISCORD_PUBLIC_KEY'])
+    if (!isValidRequest) {
+        res.status(401).end('Bad request signature')
+    }
     res.json({
         body: req.body,
         query: req.query,
