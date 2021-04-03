9c298e9a049a2236c16799b964d85a94d46085d3
Nick Holmstedt <holmstn@gmail.com>
2021-04-03 18:16:54 -0400

    Revert alternate treatment for stats at smaller screen sizes

--- src/components/Stat.vue
+++ src/components/Stat.vue
@@ -1,12 +1,8 @@
 <template>
-  <p class="hidden lg:block">
+  <p>
     {{ label }}
     <span class="text-right">{{ stat }}</span>
   </p>
-  <div class="lg:hidden flex flex-col pb-2">
-    <div class="font-bold underline">{{ label }}</div>
-    <div>{{ stat }}</div>
-  </div>
 </template>
 
 <script>
